9098a325c8873a5495259a9f734fd5d84b45ffc1
Guilherme Silva <guisilva.master@gmail.com>
2018-06-18 20:34:04 -0300

    Implements create method for product

--- src/database/model/product.js
+++ src/database/model/product.js
@@ -1,5 +1,13 @@
 const table = 'pro_products'
 
+const fields = [
+  'pro_id',
+  'pro_name',
+  'pro_description',
+  'pro_price',
+  'pro_quantity'
+]
+
 export default connection => {
   return {
     all: () => {
@@ -8,7 +16,32 @@ export default connection => {
           if (error) {
             reject(error)
           }
-          resolve(results)
+          resolve({ product: results })
+        })
+      })
+    },
+    create: (params) => {
+      return new Promise((resolve, reject) => {
+        let insertedField = []
+        let insertedBind = []
+        let insertedValue = []
+
+        Object.keys(params).forEach((key) => {
+          console.log(fields)
+          if (fields.includes(key)) {
+            insertedField.push(key)
+            insertedBind.push('?')
+            insertedValue.push(params[key])
+          }
+        })
+
+        connection.query(`INSERT INTO ${table} (${insertedField.join(', ')}) VALUES(${insertedBind.join(', ')})`, insertedValue, (error, results) => {
+          if (error) {
+            reject(error)
+          }
+          console.log(results)
+
+          resolve({ product: Object.assign({}, params, { pro_id: results.insertId }) })
         })
       })
     }
--- src/http/product.js
+++ src/http/product.js
@@ -6,7 +6,16 @@ const router = Router()
 router.get('/', async (req, res) => {
   try {
     const products = await db.product.all()
-    res.status(200).json({ products })
+    res.status(200).json(products)
+  } catch (error) {
+    res.send(error)
+  }
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const products = await db.product.create(req.body)
+    res.status(200).json(products)
   } catch (error) {
     res.send(error)
   }
