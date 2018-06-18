8cdbaa1d8eed33c89e27c31e7f2f5e94eb0bc56c
Guilherme Silva <guisilva.master@gmail.com>
2018-06-18 19:42:45 -0300

    Fix binding of connection variables

--- src/database/index.js
+++ src/database/index.js
@@ -4,8 +4,8 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USERNAME,
-  password: process.env.DB_DATABASE,
-  database: process.env.DB_PASSWORD
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 })
 
 export default connection
