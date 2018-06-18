0954c74ec55a9596356bfe22bee024e242a93d19
Guilherme Silva <guisilva.master@gmail.com>
2018-06-18 16:53:09 -0300

    Add body-parser middleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 
 // Create new instance of Express
 const app = express()
+
+// get and parse body of request
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 export default app
