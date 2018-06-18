844dfa084e6778ac6307fd18db68de5d33dc3897
Guilherme Silva <guisilva.master@gmail.com>
2018-06-18 16:38:05 -0300

    Add and configure babel to enable ES6 features

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,16 @@
 module.exports = {
-  "extends": "standard"
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'standard'
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
 };
