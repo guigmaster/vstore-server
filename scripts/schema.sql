f41d472cda71e061d0b16c8034e94f272c6b0501
Guilherme Silva <guisilva.master@gmail.com>
2018-06-18 18:08:47 -0300

    Create base structure dump of database

---
+++ scripts/schema.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.16  Distrib 10.3.6-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: vstore__development
+-- ------------------------------------------------------
+-- Server version	10.3.6-MariaDB-1:10.3.6+maria~sid-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `vstore__development`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `vstore__development` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `vstore__development`;
+
+--
+-- Table structure for table `pro_products`
+--
+
+DROP TABLE IF EXISTS `pro_products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pro_products` (
+  `pro_id` int(11) NOT NULL AUTO_INCREMENT,
+  `pro_name` varchar(80) NOT NULL,
+  `pro_description` text DEFAULT NULL,
+  `pro_price` double(10,2) NOT NULL,
+  `pro_quantity` int(11) NOT NULL,
+  `pro_created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `pro_updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  PRIMARY KEY (`pro_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-06-18 18:05:53
