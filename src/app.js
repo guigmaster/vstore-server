04411ce88f80c1d5c2991ff01caa8ec1037116a0
Guilherme Silva <guisilva.master@gmail.com>
2018-06-18 16:55:01 -0300

    Config cors middleware to API

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import cors from 'cors'
 
 // Create new instance of Express
 const app = express()
@@ -7,5 +8,10 @@ const app = express()
 // get and parse body of request
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+// Enable cors from any origin
+const corsMiddleware = cors({ origin: '*', preflightContinue: true })
+app.use(corsMiddleware)
+app.options('*', corsMiddleware)
 
 export default app
